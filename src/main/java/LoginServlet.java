47fe0eea24b4cde12aa12f68e8ef871f22a57614
amruta.vatare <vatareamruta615@gmail.com>
2022-10-19 23:00:02 +0530

    implemented name validation using regex

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -23,14 +23,36 @@ public class LoginServlet extends HttpServlet {
         String pwd = req.getParameter("pwd");
         String userID = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
-        if(userID.equals(user) && password.equals(pwd)){
-            req.setAttribute("user",user);
-            req.getRequestDispatcher("LoginSuccess.jsp").forward(req,resp);
-        }else {
-            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
-            PrintWriter out = resp.getWriter();
-            out.println("<font color = red> Either username or password is wrong</font>");
-            rd.include(req,resp);
+        if(validateUser(user)){
+            if(userID.equals(user) && password.equals(pwd)){
+                setSuccessResponse(req,resp,user);
+            }else {
+                String error = "<font color = red> Either username or password is wrong</font>";
+                setErrorResponse(req, resp,error);
+            }
         }
+        else{
+            String error = "<font color = red> username must be start with capital and have minimum 3 digits</font>";
+            setErrorResponse(req, resp,error);
+        }
+    }
+
+    // sets the output error http response
+    private void setErrorResponse(HttpServletRequest req, HttpServletResponse resp, String errorHtmlMsg)
+            throws ServletException, IOException
+    {
+        RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+        PrintWriter out = resp.getWriter();
+        out.println(errorHtmlMsg);
+        rd.include(req,resp);
+    }
+    private void setSuccessResponse(HttpServletRequest req, HttpServletResponse resp,String user)
+            throws ServletException, IOException{
+        req.setAttribute("user",user);
+        req.getRequestDispatcher("LoginSuccess.jsp").forward(req,resp);
+    }
+
+    private static boolean validateUser(String user){
+        return user.matches("^[A-Z]+[a-zA-Z]{3,8}");
     }
 }
