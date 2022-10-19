00ced7a145cc94734bfe4141a2bf1003117c7fc4
amruta.vatare <vatareamruta615@gmail.com>
2022-10-19 23:34:05 +0530

    implemented password validation using regex

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -13,7 +13,7 @@ import java.io.PrintWriter;
         urlPatterns = {"/LoginServlet"},
         initParams = {
                 @WebInitParam(name="user",value = "Amruta"),
-                @WebInitParam(name = "password",value = "bridgelabz")
+                @WebInitParam(name = "password",value = "Bridgelabz#1")
         }
 )
 public class LoginServlet extends HttpServlet {
@@ -21,19 +21,32 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String user = req.getParameter("user");
         String pwd = req.getParameter("pwd");
-        String userID = getServletConfig().getInitParameter("user");
-        String password = getServletConfig().getInitParameter("password");
-        if(validateUser(user)){
-            if(userID.equals(user) && password.equals(pwd)){
+
+        if(!validateUser(user))
+        {
+            String error = "<font color = red> username must be start with capital and have minimum 3 digits</font>";
+            setErrorResponse(req, resp,error);
+        }
+        else if(!validatePassword(pwd)){
+            String error = "<font color = red> password rules " +
+                    "1. min 8 chars" +
+                    "2. at least 1 upper case, at least 1 lower case,at least 1 number" +
+                    "3. at least 1 special character" +
+                    " </font>";
+            setErrorResponse(req, resp,error);
+        }
+        else
+        {
+            // Authenticate user
+            if(Authenticate(user, pwd))
+            {
                 setSuccessResponse(req,resp,user);
-            }else {
+            }
+            else
+            {
                 String error = "<font color = red> Either username or password is wrong</font>";
                 setErrorResponse(req, resp,error);
             }
-        }
-        else{
-            String error = "<font color = red> username must be start with capital and have minimum 3 digits</font>";
-            setErrorResponse(req, resp,error);
         }
     }
 
@@ -52,7 +65,19 @@ public class LoginServlet extends HttpServlet {
         req.getRequestDispatcher("LoginSuccess.jsp").forward(req,resp);
     }
 
+    private boolean Authenticate(String reqUserName, String reqPassword) throws ServletException, IOException{
+        String userID = getServletConfig().getInitParameter("user");
+        String password = getServletConfig().getInitParameter("password");
+        if(userID.equals(reqUserName) && password.equals(reqPassword)){
+            return true;
+        }
+        return  false;
+    }
+
     private static boolean validateUser(String user){
         return user.matches("^[A-Z]+[a-zA-Z]{3,8}");
+    }
+    private static boolean validatePassword(String password){
+        return password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[-+_!@#$%^&*.,?]).+$");
     }
 }
